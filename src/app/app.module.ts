811419c16e74132af422cae223f04ce4d9d03c96
Nahuel <nsavedra97@gmail.com>
2022-05-31 21:54:21 -0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    ListaProductosComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/routes/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { ListaProductosComponent } from '../components/lista-productos/lista-productos.component';
+import { LoginComponent } from '../components/login/login.component';
+import { ProductoComponent } from '../components/producto/producto.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'producto', component: ProductoComponent},
+  { path: 'productos', component: ListaProductosComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
