a4adb1072526e57ea0c4c76042e3d802b3567e4a
Ezequiel <71356016+exequii@users.noreply.github.com>
2022-06-03 16:55:39 -0300

    Router + Productos mediante Objeto

--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-productos',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProductosComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+
+  constructor(protected router:Router) { }
 
   ngOnInit(): void {
+
+    this.products = [
+      {
+        name: "Apple Watch",
+        price: 40,
+        details: "El mejor reloj del mercado. Combina delicadez y tecnologia en tu mano siempre.",
+        img: "https://www.pngmart.com/files/13/Smartwatch-Gadget-PNG-Clipart.png",
+        ofert: true,
+        desc: 10,
+      },
+      {
+        name: "Xiaomi Watch",
+        price: 80,
+        details: "GPS incorporado. Notificaciones. Mas de 90 modos de deporte!.",
+        img: "https://www.pngmart.com/files/13/Smartwatch-Gadget-PNG-Clipart.png",
+        ofert: false,
+        desc: 20,
+      }
+    ]
   }
 
 }
---
+++ src/app/components/lista-productos/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+
+  name: String;
+  price: Number;
+  details: String;
+  img: String;
+  ofert: Boolean;
+  desc: Number;
+
+}
